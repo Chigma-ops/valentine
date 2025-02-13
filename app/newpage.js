5286bddf26a1477bdc9c8bc8e9e4d5ff88694a57
Chinguun Toivgoo <chinguuntoivgoo@Chinguuns-MacBook-Pro.local>
2025-02-13 14:09:21 +0800

    comment uldeev

--- app/newpage.js
+++ app/newpage.js
@@ -1,29 +1,31 @@
 "use client";  
 
 import React, { useState } from "react";
+
 const UseStateHook = () => {
-    const [color, setColor] = useState(" ");
-    return (
-        <div className={`flex space-x-4 w-full h-screen bg-${color}-800`}>
-            <p>Change color:</p>
-            <button className="border bg-red-800 py-4 px-6" onClick={() => {
-                setColor("red")
-            }}>
-             Red
-            </button>
-            <button className="border bg-blue-800 py-4 px-6" onClick={() => {
-                setColor("blue")
-            }}>
-             Blue
-            </button>
-            <button className="border bg-green-800 py-4 px-6" onClick={() => {
-                setColor("green")
-            }}>
-             Green
-            </button>
-        </div>
-    
+  const [name] = useState('Taylor'); 
+  const [friendName] = useState('Chinguun'); 
+  const [showName, setShowName] = useState(false); 
+  const [showFriendName, setShowFriendName] = useState(false); 
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-full bg-gray-100">
+      <button 
+        onClick={() => setShowName(prevShowName => !prevShowName)} 
+        className="bg-indigo-500 text-white font-bold py-2 px-4 rounded shadow-xl transition duration-300 ease-in-out transform hover:bg-green-600 hover:scale-105 mb-4"
+      >
+        Sigma sigma
+      </button>
+      {showName && <p className="mt-4 text-xl text-black">{name}</p>}
+      <button 
+        onClick={() => setShowFriendName(prevShowFriendName => !prevShowFriendName)} 
+        className="bg-green-500 text-white font-bold py-2 px-4 rounded shadow-xl transition duration-300 ease-in-out transform hover:bg-blue-600 hover:scale-105 mt-4"
+      >
+        Toggle Friend Name
+      </button>
+      {showFriendName && <p className="mt-4 text-xl text-black">{friendName}</p>}
+    </div>
   );
 };
 
 export default UseStateHook;
+
